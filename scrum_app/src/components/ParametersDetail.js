c8ce31a5991eb936149e8f0d3b0c8d7c8c462105
Yann Reibel <reibelyann@gmail.com>
2022-04-21 12:42:09 +0200

    Displaying all the fields in the ParametersDetail component

--- scrum_app/src/components/ParametersDetail.js
+++ scrum_app/src/components/ParametersDetail.js
@@ -17,26 +17,26 @@ const ParametersDetail = ({hour}) => {
 
                <div className="Parameter-all">
                    <div className="Label2">Sunrise</div>
-                   <div className="value2">p</div>
+                   <div className="value2">{hour.sunrise}</div>
                </div>
 
                <div className="Parameter-all">
                    <div className="Label3">Sunset</div>
-                   <div className="value3">m</div>
+                   <div className="value3">{hour.sunset}</div>
                 </div>
                <div className="Parameter-all">
                   <div className="Label4">Coverage</div> 
-                  <div className="value4">l</div>
+                  <div className="value4">{hour.cloud_coverage}</div>
                 </div>
 
                <div className="Parameter-all">
                    <div className="Label5">Wind speed</div>
-                   <div className="value5">k</div>
+                   <div className="value5">{hour.wind_speed}</div>
                 </div>
                 
                <div className="Parameter-all">
                    <div className="Label6">Wind direction</div>
-                   <div className="value6">j</div>
+                   <div className="value6">{hour.wind_direction}</div>
                </div>
       </div> 
         );
